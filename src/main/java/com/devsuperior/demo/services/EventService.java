8fbd844bcd742150922d4e528e369354cb3b7d72
Allan-Borges <alanborges195@gmail.com>
2024-01-10 10:23:48 -0300

    All testing event

---
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -0,0 +1,48 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.CityRepository;
+import com.devsuperior.demo.repositories.EventRepository;
+import com.devsuperior.demo.services.exceptions.DatabaseException;
+import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+
+        try {
+            Event entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity.setUrl(dto.getUrl());
+            entity.setDate(dto.getDate());
+            City city = cityRepository.getReferenceById(dto.getCityId());
+            entity.setCity(city);
+            entity = repository.save(entity);
+            return new EventDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
+
+    }
+}
